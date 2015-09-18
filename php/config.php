8a6ae9faad064e42b6b1f2c19abefc8bf58298d4
ksanderpugin <ksander.nrj@gmail.com>
2015-09-18 17:01:16 +0300

    dbconfig

--- php/config.php
+++ php/config.php
@@ -1,10 +1,10 @@
 <?php
 
 class Config {
-    const SQL_SERVER_HOST = 'localhost';
-    const SQL_SERVER_USER_NAME = 'root';
-    const SQL_SERVER_USER_PASS = 'PavPat2010';
-    const SQL_SERVER_BD_NAME = 'fiteat';
+    const SQL_SERVER_HOST = 'zoostyle.mysql.ukraine.com.ua';
+    const SQL_SERVER_USER_NAME = 'zoostyle_fe';
+    const SQL_SERVER_USER_PASS = '6kz7f4hx';
+    const SQL_SERVER_BD_NAME = 'zoostyle_fe';
 
     const FB_APP_ID = '515962271900994';
     const FB_SECURE_CODE = '0c726bc40c1393e39b14de3b4743b73c';
