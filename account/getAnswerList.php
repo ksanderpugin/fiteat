d73df7f9ac2e2bac9494a124d3ef61d112a83a46
ksanderpugin <ksander.nrj@gmail.com>
2016-03-28 16:36:48 +0300

    easy search prods

--- account/getAnswerList.php
+++ account/getAnswerList.php
@@ -1,5 +1,31 @@
 <?php
 
-$data = file_get_contents(__DIR__ . "/answerList.json");
+include_once $_SERVER['DOCUMENT_ROOT'] . '/php/config.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/php/sql.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/php/user.php';
 
-echo $data;
+$key = array_key_exists('word', $_GET) ? $_GET['word'].'%' : printDefAndExit();
+
+if (!User::getInst()->isAuthorized()) printDefAndExit();
+
+$sql = SQL::getInst();
+
+$result = $sql->query("SELECT id, name FROM products WHERE name LIKE ':word' LIMIT 10",[
+		["name" => ":word", "val" => $key, "type" => SQL::PARAM_STR]
+]);
+
+$list = [];
+if (!empty($result)) foreach ($result as $row) {
+	$list[] = ["id" => $row['id'], "value" => $row['name']];
+}
+
+echo json_encode(["answers" => $list]);
+
+//$data = file_get_contents(__DIR__ . "/answerList.json");
+
+//echo $data;
+
+function printDefAndExit() {
+	echo json_encode([]);
+	exit;
+}
